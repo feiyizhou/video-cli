06465f1f91947f99b7a9555930aef0dc2b954c58
feiyizhou <1491877304@qq.com>
2022-12-13 20:21:54 +0800

    1.Edit the main.go

--- main.go
+++ main.go
@@ -11,21 +11,4 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	//locate, err := time.LoadLocation("Asia/Shanghai")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//c := cron.New(cron.WithLocation(locate), cron.WithSeconds())
-	//_, err = c.AddFunc("*/3 * * * * *", test)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//c.Start()
-	//defer c.Stop()
-	//select {}
 }
-
-//func test() {
-//	fmt.Println("every 3 seconds running ...")
-//}
